b4479e592859ab68fef44d60323db4e7c7e5ba6f
claudiasgithub2 <acsalituro@gmail.com>
2023-11-24 14:52:38 -0300

    carrito funcionando

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { useState } from 'react'
 
-function ItemCount() {
+function ItemCount( {cantidad, handleRestar, handleSumar, handleAgregar} ) {
 
-    const [cantidad, setCantidad] = useState(1);
+  
 
 
 
@@ -11,7 +11,7 @@ function ItemCount() {
     <div className="container mt-3">
       <div className="row align-items-center">
         <div className="col">
-          <button className="btn btn-primary">
+          <button className="btn btn-primary " onClick={handleRestar}>
             -
           </button>
         </div>
@@ -19,14 +19,19 @@ function ItemCount() {
           <p>{cantidad}</p>
         </div>
         <div className="col">
-          <button className="btn btn-primary">
+          <button className="btn btn-primary" onClick={handleSumar}>
             +
           </button>
         </div>
       </div>
       <div className="row mt-3">
         <div className="col">
-          <button className="btn btn-success">Agregar al carrito</button>
+          <button className="btn btn-success bi bi-cart2 " onClick={handleAgregar}>
+            Agregar al carrito
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart2" viewBox="0 0 16 16">
+              <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0" />
+            </svg>
+          </button>
         </div>
       </div>
     </div>
--- src/components/ItemDetalle.jsx
+++ src/components/ItemDetalle.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ItemCount from './ItemCount'
 
 const ItemDetalle = (item) => {
+
+
+  const [cantidad, setCantidad] = useState(1);
+
+  const handleRestar = () => {
+    cantidad > 1 && setCantidad(cantidad - 1)
+
+  }
+
+  const handleSumar = () => {
+    cantidad < item.stock && setCantidad(cantidad + 1)
+  }
+
+  const handleAgregar = () => {
+    console.log ({...item, cantidad});
+  }
+
+
+
+
   return (
     <div className="card mb-6 shadow p-3 bg-body-tertiary rounded d-flex justify-content-center" style={{ maxWidth: '600px', margin: '0 auto', marginTop: '10vh' }}>
       <div className="row g-0 d-flex justify-content-center align-items-center">
@@ -14,7 +34,7 @@ const ItemDetalle = (item) => {
             <p className="card-text">Precio: ${item.precio}</p>
             <p className="card-text">Categoría: {item.categoria}</p>
             <p className="card-text">{item.descripcion}</p>
-            <ItemCount />
+            <ItemCount cantidad={cantidad} handleRestar={handleRestar} handleSumar={handleSumar} handleAgregar={handleAgregar} />
           </div>
         </div>
       </div>
